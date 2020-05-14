b40863ade7bbcba0755c102faeeba0c39f49dedf
Max Magee <max.r.magee@gmail.com>
2020-05-14 10:19:09 -0500

    Added enabledScreens call to increase performance.

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { enableScreens } from "react-native-screens";
+
+enableScreens();
 
 export default function App() {
   return (
